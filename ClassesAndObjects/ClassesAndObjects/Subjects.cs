14f9db0a1943f378a21d186ba3a540327380cd49
lab146 <lab146@Lab146-PC40>
2018-03-27 15:14:32 +0200

    Remove Function of Subjects

--- ClassesAndObjects/ClassesAndObjects/Subjects.cs
+++ ClassesAndObjects/ClassesAndObjects/Subjects.cs
@@ -7,30 +7,53 @@ using System.Threading.Tasks;
 
 namespace ClassesAndObjects
 {
-   public class Subjects : DictionaryBase
+    public class Subjects : DictionaryBase
     {
+
+
         public int Add(Subject newSubject)
         {
-            int returnVlaue = -2;
+            int returnValue = -2;
 
             if (newSubject != null)
             {
-                returnVlaue = 0;
+                returnValue = 0;
                 Dictionary.Add(newSubject.Code, newSubject);
             }
             else
             {
                 if (Dictionary.Contains(newSubject))
                 {
-                    returnVlaue = -1;
+                    returnValue = -1;
                 }
                 else
                 {
 
                 }
             }
-            
-            return returnVlaue;
+
+            return returnValue;
+        }
+
+        public int Remove(string subjectCode)
+        {
+            int returnValue = 0;
+
+            if (Dictionary.Count <= 0)
+            {
+                returnValue = -1;
+                throw new ListEmptyException("Subject collection is empty, nothing to remove");
+            }
+            else
+            {
+                if (Dictionary.Contains(subjectCode))
+                {
+                    Dictionary.Remove(subjectCode);
+                }
+                    
+            }
+
+            return returnValue;
         }
     }
 }
